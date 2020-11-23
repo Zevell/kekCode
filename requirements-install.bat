b4565aa0b6ae1e87e9f31840f36d1eaaedc8ce1f
Zevell <zevalgaming@gmail.com>
2020-11-23 12:58:31 +1000

    Fix requirements

--- requirements-install.bat
+++ requirements-install.bat
@@ -1,8 +1,5 @@
 @echo off
-echo DO NOT run as admin
-npm install
-npm i discord.js @discordjs/opus -g
-npm i nodemon -g
+npm install .
 npm i windows-build-tools -g
 npm i node-gyp -g
 npm i ffmpeg-static -g
